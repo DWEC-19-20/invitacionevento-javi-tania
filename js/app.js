cc751651e99706e996b24553cb689c83997c39de
unknown <javimoya11@gmail.com>
2020-01-15 09:35:44 +0100

    carpetas y .js creados

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* 
+    1. querría añadir nombres a la lista de invitados para saber quien asistirá.
+    2. querría poder confirmar invitados al evento para saber quien finalmente irá
+    3. poder editar el nombre de cada asistente al evento para corregir errores
+    4. querría poder filtrar los invitados para mostrar solo los que tienen confirmada la asistencia para visualizarlo mejor
+    5. querría poder eliminar invitados al evento que se hayan dado de baja
+    6. me confirme antes de borrar un invitado al evento para evitar cometer un error
+    7. querría que no se pudiera repetir el nombre de un invitado al evento ni añadir nombres vacios para evitar errores
+*/
+
