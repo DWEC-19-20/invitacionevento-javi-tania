4ff52d5e11c33514b97f7d5287e0eaca7e16fea2
unknown <javimoya11@gmail.com>
2020-01-16 12:13:19 +0100

    funcion remove a medias

--- js/app.js
+++ js/app.js
@@ -8,3 +8,17 @@
     7. querría que no se pudiera repetir el nombre de un invitado al evento ni añadir nombres vacios para evitar errores
 */
 
+/* Hay que crear cada elemento uno por uno (el 'li', el 'span', etc...) */
+
+function borrarNombre(num) {
+    var elementoLista = document.getElementById("nombre"+num+"");
+    var elementoBoton = document.getElementbyId("boton-borrar"+num+"");
+    elementoBoton.addEventListener("click", function () {
+        var confirma = confirm("¿Seguro que quieres borrar este nombre?");
+        if (confirma) {
+            document.removeChild(elementoLista);
+        }
+    });
+}
+
+borrarNombre("1");
