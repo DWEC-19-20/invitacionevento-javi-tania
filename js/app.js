e64b83cfff09f9136c5b9f1bee3edba90d93623a
unknown <javimoya11@gmail.com>
2020-02-03 11:33:42 +0100

    Refiniquitado

--- js/app.js
+++ js/app.js
@@ -14,38 +14,38 @@ enviar.addEventListener("click", añadirInvitado);
 
 function añadirInvitado(event) {
 
-  inviteurLi = document.createElement("LI");
-  inviteurSpan = document.createElement("span");
-  $nombre = document.getElementById("invitado").value;
-  if (repes($nombre))
-    var nombre = document.createTextNode($nombre);
-  else {
-    alert("No metas un nombre vacío o repetido");
+    inviteurLi = document.createElement("LI");
+    inviteurSpan = document.createElement("span");
+    $nombre = document.getElementById("invitado").value;
+    if (repes($nombre))
+        var nombre = document.createTextNode($nombre);
+    else {
+        alert("No metas un nombre vacío o repetido");
+        event.preventDefault();
+    }
+
+    document.getElementById("invitado").value = "";
+    inviteurSpan.appendChild(nombre);
+    document.getElementById("invitedList").appendChild(inviteurLi);
+    inviteurLabel = document.createElement("label");
+    nombreLabel = document.createTextNode("Confirmed");
+    inviteurLabel.appendChild(nombreLabel);
+    check = document.createElement("INPUT");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("id", "confirmation");
+    inviteurLabel.appendChild(check);
+    inviteurButt = document.createElement("button");
+    inviteurButt2 = document.createElement("button");
+    inviteurButt.textContent = "Edit";
+    inviteurButt2.textContent = "Remove";
+
+    inviteurLi.appendChild(inviteurSpan);
+    inviteurLi.appendChild(inviteurLabel);
+    inviteurLi.appendChild(inviteurButt);
+    inviteurLi.appendChild(inviteurButt2);
+
+
     event.preventDefault();
-  }
-
-  document.getElementById("invitado").value = "";
-  inviteurSpan.appendChild(nombre);
-  document.getElementById("invitedList").appendChild(inviteurLi);
-  inviteurLabel = document.createElement("label");
-  nombreLabel = document.createTextNode("Confirmed");
-  inviteurLabel.appendChild(nombreLabel);
-  check = document.createElement("INPUT");
-  check.setAttribute("type", "checkbox");
-  check.setAttribute("id", "confirmation");
-  inviteurLabel.appendChild(check);
-  inviteurButt = document.createElement("button");
-  inviteurButt2 = document.createElement("button");
-  inviteurButt.textContent = "Edit";
-  inviteurButt2.textContent = "Remove";
-
-  inviteurLi.appendChild(inviteurSpan);
-  inviteurLi.appendChild(inviteurLabel);
-  inviteurLi.appendChild(inviteurButt);
-  inviteurLi.appendChild(inviteurButt2);
-
-
-  event.preventDefault();
 }
 
 /* Función para no poder nitroducir un nombre en blanco ni un repetido */
@@ -54,11 +54,11 @@ function añadirInvitado(event) {
 
 function repes($nombre) {
 
-  const nombrecitos = document.querySelectorAll("#invitedList span");
-  for (let i = 0; i < nombrecitos.length; i++) {
-    if ($nombre == nombrecitos[i].textContent || ($nombre === ""))
-      return false;
-  } return true;
+    const nombrecitos = document.querySelectorAll("#invitedList span");
+    for (let i = 0; i < nombrecitos.length; i++) {
+        if ($nombre == nombrecitos[i].textContent || ($nombre === ""))
+            return false;
+    } return true;
 }
 
 
@@ -69,54 +69,55 @@ const listUl = document.querySelector("ul.invitedList");
 
 
 listUl.addEventListener('click', (event) => {
-  if (event.target.tagName == 'BUTTON' && event.target.textContent == "Remove") {
-    let li = event.target.parentNode;
-    let ul = li.parentNode;
-    let confirmacion = confirm("¿Estás seguro que quieres borrar a esta persona?");
-    if (confirmacion) {
-      ul.removeChild(li);
+    if (event.target.tagName == 'BUTTON' && event.target.textContent == "Remove") {
+        let li = event.target.parentNode;
+        let ul = li.parentNode;
+        let confirmacion = confirm("¿Estás seguro que quieres borrar a esta persona?");
+        if (confirmacion) {
+            ul.removeChild(li);
+        }
     }
-  }
 });
 
 /* Evento Editar */
 
 
 listUl.addEventListener('click', (event) => {
-  if (event.target.tagName == 'BUTTON' && event.target.textContent == "Edit") {
-    let li = event.target.parentNode;
-    let span = li.firstChild;
-    span.textContent = "";
-    span = document.createElement("INPUT");
-    span.setAttribute("type", "text");
-    span.setAttribute("placeholder", "Enter para guardar");
-    li.prepend(span);
-    event.target.textContent = "Save";
+    if (event.target.tagName == 'BUTTON' && event.target.textContent == "Edit") {
+        let li = event.target.parentNode;
+        let span = li.firstChild;
+        span.textContent = "";
+        span = document.createElement("INPUT");
+        span.setAttribute("type", "text");
+        span.setAttribute("placeholder", "Enter para guardar");
+        li.prepend(span);
+        event.target.textContent = "Save";
 
         var boton = listUl.querySelectorAll("BUTTON");
 
-    span.addEventListener("keyup", function (event) {
-      if (event.keyCode === 13) {
-        let nombre = li.firstChild.value;
-        if (repes(nombre))
-          span = document.createElement("SPAN");
-        else {
-          alert("No metas un nombre vacío o repetido");
-          event.preventDefault();
-        }
+        span.addEventListener("keyup", function (event) {
+            if (event.keyCode === 13) {
+                let nombre = li.firstChild.value;
+                if (repes(nombre)) {
+                    span = document.createElement("SPAN"); for (let i = 0; i < boton.length; i++) {
+                        if (boton[i].textContent == "Save") {
+                            boton[i].textContent = "Edit";
+                        }
+                    }
+                }
+                else {
+                    alert("No metas un nombre vacío o repetido");
+                    event.preventDefault();
+                }
 
-        span.textContent = nombre;
-        li.prepend(span);
-        li.removeChild(span.nextSibling);
-        
-        for (let i = 0; i < boton.length; i++) {
-          if (boton[i].textContent == "Save") {
-              boton[i].textContent = "Edit";
-          }
-      }
-      }
-    });
-  }
+                span.textContent = nombre;
+                li.prepend(span);
+                li.removeChild(span.nextSibling);
+
+
+            }
+        });
+    }
 });
 
 
@@ -130,17 +131,17 @@ listUl.addEventListener('change', confirmarInvitado);
 
 
 function confirmarInvitado(event) {
-  if (event.target.tagName == 'INPUT') {
-    let li = event.target.parentNode;
-    let ul = li.parentNode;
+    if (event.target.tagName == 'INPUT') {
+        let li = event.target.parentNode;
+        let ul = li.parentNode;
 
-    if (event.target.checked) {
-      ul.setAttribute("class", "responded");
+        if (event.target.checked) {
+            ul.setAttribute("class", "responded");
+        }
+        else {
+            ul.setAttribute("class", "notresponded");
+        }
     }
-    else {
-      ul.setAttribute("class", "notresponded");
-    }
-  }
 }
 
 /* Evento/Función filtrar */
